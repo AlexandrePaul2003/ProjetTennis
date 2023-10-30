409d2244c9afa30c5c6ce0572b582e494a876dfa
AlexandrePaul2003 <alexandre.paul@condorcet.be>
2023-10-30 18:57:21 +0100

    Added creation of players (M and F)

--- src/be/PaulA/pojo/Main.java
+++ src/be/PaulA/pojo/Main.java
@@ -13,8 +13,8 @@ public class Main {
 	public static void main(String[] args) {
 		PlayerDAO pDAO = new PlayerDAO(ProjectConnection.getInstance());
 		System.out.println(pDAO.find(1));
-		/*
-		String[] noms = {"Paul", "Pignes", "Poirreaux","Foret","Cappelen","Djokovic","Nadal","Federer","Messi","Ronaldo","Ibra","Neymar","Rony","Dayot"};		
+		
+		/*String[] noms = {"Paul", "Pignes", "Poirreaux","Foret","Cappelen","Djokovic","Nadal","Federer","Messi","Ronaldo","Ibra","Neymar","Rony","Dayot"};		
 		String[] prenoms = {"Alexandre", "Paul", "Hercule","Isabelle","Christiane","Novak","Rafa","Roger","Leo","Christiano","ZLATAN","Jean","GOAT","Upamecado"};
 		String[] nat = {"Belge", "Francais", "Néérlandais","Allemand","Espagnol","Portugais","Italien","Ruskov","Argentin","Brésilien","Suédois","JeanBonBeurre","Américain","Canadien"};
 		List<Player> p = new ArrayList<Player>();
@@ -22,10 +22,12 @@ public class Main {
 		for(int i = 0;i<128;i++) {
 			p.add(new Player(noms[rnd.nextInt(12)],prenoms[rnd.nextInt(12)],nat[rnd.nextInt(12)],i,"M"));
 		}
+		for(int i = 0;i<128;i++) {
+			p.add(new Player(noms[rnd.nextInt(12)],prenoms[rnd.nextInt(12)],nat[rnd.nextInt(12)],i,"F"));
+		}
 		for(Player player : p) {
-			System.out.println(player.getFirstname()+" " + player.getLastname()+" "+player.getNationnality()+" "+player.getRank());
+			pDAO.create(player);
 		}*/
-		pDAO.create(new Player("Paul","Alexandre","Belge",1,"M"));
 		
  	}
 
