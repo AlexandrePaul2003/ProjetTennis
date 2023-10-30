40564c04e43aae83cf73877235f9995637f649f6
AlexandrePaul2003 <alexandre.paul@condorcet.be>
2023-10-30 19:10:41 +0100

    Added ref creation in the database

--- src/be/PaulA/pojo/Main.java
+++ src/be/PaulA/pojo/Main.java
@@ -12,22 +12,30 @@ public class Main {
 
 	public static void main(String[] args) {
 		PlayerDAO pDAO = new PlayerDAO(ProjectConnection.getInstance());
-		System.out.println(pDAO.find(1));
+		RefereeDAO rDAO = new RefereeDAO(ProjectConnection.getInstance());
 		
-		/*String[] noms = {"Paul", "Pignes", "Poirreaux","Foret","Cappelen","Djokovic","Nadal","Federer","Messi","Ronaldo","Ibra","Neymar","Rony","Dayot"};		
+		String[] noms = {"Paul", "Pignes", "Poirreaux","Foret","Cappelen","Djokovic","Nadal","Federer","Messi","Ronaldo","Ibra","Neymar","Rony","Dayot"};		
 		String[] prenoms = {"Alexandre", "Paul", "Hercule","Isabelle","Christiane","Novak","Rafa","Roger","Leo","Christiano","ZLATAN","Jean","GOAT","Upamecado"};
 		String[] nat = {"Belge", "Francais", "Néérlandais","Allemand","Espagnol","Portugais","Italien","Ruskov","Argentin","Brésilien","Suédois","JeanBonBeurre","Américain","Canadien"};
 		List<Player> p = new ArrayList<Player>();
 		Random rnd = new Random();
-		for(int i = 0;i<128;i++) {
+		/*for(int i = 0;i<128;i++) {
 			p.add(new Player(noms[rnd.nextInt(12)],prenoms[rnd.nextInt(12)],nat[rnd.nextInt(12)],i,"M"));
 		}
 		for(int i = 0;i<128;i++) {
 			p.add(new Player(noms[rnd.nextInt(12)],prenoms[rnd.nextInt(12)],nat[rnd.nextInt(12)],i,"F"));
 		}
-		for(Player player : p) {
+		/*for(Player player : p) {
 			pDAO.create(player);
 		}*/
+		
+		List<Referee> r = new ArrayList<Referee>();
+		for(int i=0;i<16;i++) {
+			r.add(new Referee(noms[rnd.nextInt(12)],"Referee"+i,"Belge"));
+		}
+		for(Referee ref : r) {
+			rDAO.create(ref);
+		}
 		
  	}
 
